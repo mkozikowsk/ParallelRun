adb9ccda03da4cde3fdff7f8f8aba459a154ad6b
Kozikowski Marcin <marcin.kozikowski@gfieast.com>
2021-01-15 17:21:05 +0100

    add test features

---
+++ ParallelRun/Features/TestFeature2.feature
@@ -0,0 +1,17 @@
+﻿Feature: TestFeature2
+
+Scenario: TestFeature2_Scenario 1 wait 4 secons
+	When I wait '4' seconds
+
+Scenario: TestFeature2_Scenario 2 wait 5 second
+	When I wait '5' seconds
+
+Scenario Outline: TestFeature2_Scenario Outline 3 wait 2 seconds
+	When I wait '2' seconds
+Examples: 
+	| index |
+	| 1     |
+	| 2     |
+	| 3     |
+	| 4     |
+	| 5     |
---
+++ ParallelRun/Features/TestFeatureSteps.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using TechTalk.SpecFlow;
+
+namespace ParallelRun.Features
+{
+    [Binding]
+    public class TestFeatureSteps
+    {
+        [When(@"I wait '(.*)' seconds")]
+        public void WhenIWaitSeconds(int waitTime)
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(waitTime*1000);
+        }
+    }
+}
